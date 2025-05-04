f9675aa96e19451f54c068269c31cc27da0c300f
Dylan Hu <D.Hu-4@student.tudelft.nl>
2025-05-04 16:18:43 +0200

    integration of duckdb wasm following the jetblack repo related to issue #19

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import DuckDB from '@jetblack/duckdb-react';
+import { VITE_BUNDLES } from './duckdbBundles';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* The jetblack github repo included wrapping it in duckdb bundle like below, but not sure how it works */}
+    {/* <DuckDB bundles={VITE_BUNDLES}> */}
+      <App />
+    {/* </DuckDB> */}
   </StrictMode>,
 )
