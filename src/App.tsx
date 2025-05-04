52588776aecde3d21c708c595174ff17abf95b90
Tsvetelina Ilieva <T.B.Ilieva@student.tudelft.nl>
2025-05-04 23:19:23 +0200

    Added file upload functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,65 @@ import DuckDB, { useDuckDB } from '@jetblack/duckdb-react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from "react";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [file, setFile] = useState<File | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
 
   // Get the DuckDB context from the hook.
   const { db, loading, error } = useDuckDB()
+
+
+  // Upload a DuckDB file
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+      let uploadedFile = null;
+
+      if (e.target.files) {
+          uploadedFile = e.target.files[0];
+      } else {
+          uploadedFile = null;
+      }
+      if (uploadedFile) {
+          setMessage(null);
+          if (uploadedFile.name.endsWith('.duckdb')) {
+              setFile(uploadedFile);
+              setMessage('File selected successfully.');
+          } else {
+              setFile(null);
+              setMessage('Please upload a valid DuckDB database file (.duckdb)');
+          }
+      }
+  }
 
   useEffect(() => {
     if (loading || !db || error) {
       return
     }
+    if(file) {
+        db.open(file).then(() => {
+            console.log("Database uploaded successfully.");
 
-    // Do something with duckdb.
+        }).catch(error => {
+            console.error(error);
+        });
+    }
 
-  }, [loading, db, error])
+  }, [loading, file, db, error])
 
   return (
     <>
+        <div>
+            <h2>Upload a DuckDB File</h2>
+
+            <input type="file" accept=".duckdb" onChange={handleFileUpload} />
+            {loading && <p>Loading DuckDB...</p>}
+            {error && <p>Error: {error.toString()}</p>}
+
+            {file && <p>Selected file: {file.name}</p>}
+            {message && <p>{message}</p>}
+        </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -45,4 +86,13 @@ function App() {
   )
 }
 
-export default App
+// Wraps App with DuckDB provider to provide context for DuckDB functionality.
+function WrappedApp() {
+    return (
+        <DuckDB>
+            <App />
+        </DuckDB>
+    );
+}
+
+export default WrappedApp;
